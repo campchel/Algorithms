b14d9cd17ad07cba5df502ef0aadfb65a6aefd7e
Chelsey Campbell <cc.chels.campbell@gmail.com>
2023-06-16 09:36:18 -0600

    Consecutive Sums

---
+++ Week9/Day5.js
@@ -0,0 +1,81 @@
+// Interview Algo given to alumni Oct 2019
+
+/* 
+    You are given a list of integers. Find all the consecutive sets of 
+    integers that adds up to the sum passed in as one of the inputs.
+*/
+
+const expected1 = [
+    [2, 5, 3, 6],
+    [3, 6, 7],
+];
+
+const expected2 = [];
+
+const expected3 = [];
+
+// Bonus:
+const expected4 = [
+    [2, 5, 3, 6],
+    [3, 6, 7],
+    [3, 6, 7, 0],
+    [3, 6, 7, 0, 0],
+];
+
+// Bonus:
+const expected5 = [
+    [-2, -5, -3, -6],
+    [-3, -6, -7],
+    [-3, -6, -7, -0],
+    [-3, -6, -7, -0, -0],
+];
+
+/*****************************************************************************/
+/**
+ * Finds all the sets of consecutive numbers that sum to the given target sum.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums Unordered nums.
+ * @param {number} targetSum
+ * @returns {Array<Array<number>>} 2d array where each nested array is a set of
+ *    consecutive numbers that add up to the given targetSum. Consecutive in
+ *    this context means the numbers whose indexes are one after the other
+ *    only.
+*/
+const nums1 = [2, 5, 3, 6, 7, 23, 12];
+const sum1 = 16;
+const nums2 = [];
+const sum2 = 5;
+const nums3 = [10, 15, 20, 35, 30];
+const sum3 = 5;
+const nums4 = [2, 5, 3, 6, 7, 0, 0, 23, 12];
+const sum4 = 16;
+const nums5 = [-2, -5, -3, -6, -7, -0, -0, -23, -12];
+const sum5 = -16;
+
+function findConsqSums(nums, targetSum) {
+    // initialize an empty Array called sumSets to store the resulting sets of consecutive numbers
+    const sumSets=[]
+
+    // iterate over the input array using a for loop
+    for(let i = 0; i < nums.length; i++) {
+    
+        let sum = nums[i]
+        
+        for(let c = i + 1; c <= nums.length; c++){
+            if(sum === targetSum){
+                sumSets.push(nums.slice(i, c))
+            }
+            sum += nums[c]
+        }
+    }
+
+    return sumSets
+}
+console.log(findConsqSums(nums1, sum1))
+console.log(findConsqSums(nums2, sum2))
+console.log(findConsqSums(nums3, sum3))
+console.log(findConsqSums(nums4, sum4))
+console.log(findConsqSums(nums5, sum5))
+
+module.exports = { findConsqSums };
